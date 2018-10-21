1862815e8c54d70d3914ca4b3fcabe9f1f47563b
Gibles <derekbohnet@gmail.com>
2018-10-21 15:37:21 -0700

    Created toggle axes class w/ TODO

---
+++ src/a2/ToggleAxes.java
@@ -0,0 +1,16 @@
+package a2;
+
+import javax.swing.AbstractAction;
+import java.awt.event.ActionEvent;
+
+@SuppressWarnings("serial")
+public class ToggleAxes extends AbstractAction {
+	public ToggleAxes (Camera c) {
+		//TODO
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent evt) {
+		//TODO
+	}
+}
